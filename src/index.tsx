79a5ba5675b7fc84d8ef246841ff0041a051ca6c
Dani Riera <daniel.riera@qatium.com>
2021-04-27 15:15:32 +0200

    Add theme to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+
+import "./theme/reset.css";
+import "./theme/variables.css";
+import "./theme/default.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
